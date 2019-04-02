97de0b2b7857c196eaf94d5fdbf90e8f0435d760
Josh Byster <joshb1050@gmail.com>
2019-04-01 21:05:01 -0500

    Worked on email CLI
    Former-commit-id: 6439b741968eda8bc810ccb07fde53b961a8a7c8

---
+++ src/commands/email.js
@@ -0,0 +1,46 @@
+// @flow
+const handleErrors = require("../utils/handleErrors");
+const fs = require("fs");
+const copy = require("recursive-copy");
+const inquirer = require("inquirer");
+
+module.exports.command = "email";
+module.exports.describe = "Sets up the email tokens to use Gmail.";
+module.exports.builder = (yargs: any) => yargs;
+const CREDENTIALS_PATH = '../../../credentials.json';
+
+var questions = [
+  {
+    type: "confirm",
+    name: "gmail_setup",
+    message:
+      "Welcome to the setup!\nBefore preceding, have you set up a Gmail account?"
+  },
+  {
+    type: "confirm",
+    name: "below_url",
+    message:
+      'Navigate to the below URL.\n In step 1, please hit the big "Enable The Gmail API" button. \nhttps://developers.google.com/gmail/api/quickstart/nodejs\n Continue?'
+  },
+  {
+    type: "confirm",
+    name: "client_config",
+    message:
+      'Click the big "Download Client Configuration" button. \n Save/copy this file into the root directory.\n Is this done?'
+  }
+];
+
+module.exports.handler = handleErrors(async (argv: {}) => {
+  console.info("");
+  await inquirer.prompt(questions);
+  if (!fs.existsSync(CREDENTIALS_PATH)) {
+    console.err("Can't find file: credentials.json in root directory")
+    // Do something
+    return;
+  }
+
+  console.log("Copying into directory...");
+  
+  fs.createReadStream('../../../credentials.json').pipe(fs.createWriteStream('./credentials.json'));
+
+});
