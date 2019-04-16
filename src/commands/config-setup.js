954eb1b7c47245d5bd745ee696a18559c4a85fb5
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-04-15 20:44:15 -0500

    better work

--- src/commands/config-setup.js
+++ src/commands/config-setup.js
@@ -1,49 +1,58 @@
 const handleErrors = require("../utils/handleErrors");
 const jsYaml = require('js-yaml')
 const fs = require('fs')
-const readline = require('readline')
+const inquirer = require("inquirer")
 
 module.exports.command = "config-setup";
 module.exports.describe =
     "Enters customizable configurations into config file";
 module.exports.builder = (yargs) => yargs;
+const CONFIG_URL = 'templates/infra-authentication-server/config/defaultroles.yml'
+
+const questions = [
+    {
+      type: "confirm",
+      name: "gauth",
+      message:
+        "Would you like to use Google Authentication for signing in?"
+    },
+    {
+      type: "confirm",
+      name: "gmail",
+      message: "Would you like to use Gmail for password recovery?"
+    },
+    {
+      type: "confirm",
+      name: "sq",
+      message: "Would you like to use a security question for password recovery?"
+    }
+];
+
+let responses = ["", "", ""];
+
+askQuestions = async () => {
+    for(let i = 0; i < questions.length; i++){
+        const response = await inquirer.prompt(questions[i])
+        responses[i] = response[Object.keys(response)[0]]
+    }
+}
 
 module.exports.handler = handleErrors(async (argv) => {
-    let interface = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-    console.log('Note that Google Authentication and Gmail both require setting up Google Oauth.')
-    interface.question('Would you like to use Google Authentication for signing in? (yes/no): ', async (choice1) => {
-        const c1 = choice1.toLowerCase()
-        if (c1 !== 'yes' && c1 !== 'no') {
-            return console.error("Invalid response. Please run again.")
-        }
-        console.log('Note that you should enable Gmail, security questions, or both for password recovery.')
-        interface.question('Would you like to use Gmail for password recovery? (yes/no): ', async (choice2) => {
-            const c2 = choice2.toLowerCase()
-            if (c2 !== 'yes' && c2 !== 'no') {
-                return console.error("Invalid response. Please run again.")
-            }
-            interface.question('Would you like to use a security question for password recovery? (yes/no): ', async (choice3) => {
-                const c3 = choice3.toLowerCase()
-                if (c3 !== 'yes' && c3 !== 'no') {
-                    return console.error("Invalid response. Please run again.")
-                }
-                if(c2 === 'no' && c3 === 'no') {
-                    return console.error("Please enable either Gmail or security questions.")
-                }
-                const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
-                configInfo.useGoogleAuth = c1 == "yes" ? true : false
-                configInfo.gmail = c2 == "yes" ? true : false
-                configInfo.security_question = c3 == "yes" ? true : false
-                const yamlStr = jsYaml.safeDump({ ...configInfo })
-                fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
-                    if (err) console.log(err, 'something went wrong')
-                    else console.log('Success!')
-                })
-                interface.close()
-            })
-        })
+    try {
+        console.log("Note that Google Authentication and Gmail both require setting up Google Oauth.")
+        await askQuestions();
+    } catch (e) {
+    return console.error(e);
+    }
+    if (!responses[1] && !responses[2])
+        return console.error("Please enable either Gmail or security questions.")
+    const configInfo = await jsYaml.safeLoad(fs.readFileSync(CONFIG_URL, 'utf8'))
+    configInfo.useGoogleAuth = responses[0]
+    configInfo.gmail = responses[1]
+    configInfo.security_question = responses[2]
+    const yamlStr = jsYaml.safeDump({ ...configInfo })
+    fs.writeFile(CONFIG_URL, yamlStr, (err) => {
+        if (err) console.log(err, 'Something went wrong')
+        else console.log('Success!')
     })
 })
