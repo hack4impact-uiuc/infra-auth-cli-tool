cadf561337a93b048c112b6fd0a9ccd59015e106
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-04-15 20:08:16 -0500

    should work

--- src/commands/config-setup.js
+++ src/commands/config-setup.js
@@ -13,37 +13,36 @@ module.exports.builder = (yargs) => yargs;
 
 module.exports.handler = handleErrors(async (argv) => {
     console.log('Note that Google Authentication and Gmail both require setting up Google Oauth.')
-    readline.question('Would you like to use Google Authentication for signing in? (yes/no)', async (choice) => {
-        const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
-        configInfo.useGoogleAuth = choice.toLowerCase() == "yes" ? true : false
-        const yamlStr = jsYaml.safeDump({ ...configInfo })
-        fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
-            if (err) console.log(err, 'something went wrong')
-            else console.log('Success!')
+    readline.question('Would you like to use Google Authentication for signing in? (yes/no):\n', async (choice1) => {
+        const c1 = choice1.toLowerCase()
+        if (c1 !== 'yes' && c1 !== 'no') {
+            return console.error("Invalid response. Please run again.")
+        }
+        console.log('Note that you should enable Gmail, security questions, or both for password recovery.')
+        readline.question('Would you like to use Gmail for password recovery? (yes/no): ', async (choice2) => {
+            const c2 = choice2.toLowerCase()
+            if (c2 !== 'yes' && c2 !== 'no') {
+                return console.error("Invalid response. Please run again.")
+            }
+            readline.question('Would you like to use a security question for password recovery? (yes/no): ', async (choice3) => {
+                const c3 = choice3.toLowerCase()
+                if (c3 !== 'yes' && c3 !== 'no') {
+                    return console.error("Invalid response. Please run again.")
+                }
+                if(c2 === 'no' && c3 === 'no') {
+                    return console.error("Please enable either Gmail or security questions.")
+                }
+                const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
+                configInfo.useGoogleAuth = c1 == "yes" ? true : false
+                configInfo.gmail = c2 == "yes" ? true : false
+                configInfo.security_question = c3 == "yes" ? true : false
+                const yamlStr = jsYaml.safeDump({ ...configInfo })
+                fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
+                    if (err) console.log(err, 'something went wrong')
+                    else console.log('Success!')
+                })
+                readline.close()
+            })
         })
-        readline.close()
     })
-    console.log('Note that you should enable Gmail, security questions, or both for password recovery.')
-    console.log('Would you like to use Gmail for password recovery? (yes/no)')
-    readline.question('',
-     async (choice) => {
-        const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
-        configInfo.gmail = choice == "yes" ? true : false
-        const yamlStr = jsYaml.safeDump({ ...configInfo })
-        fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
-            if (err) console.log(err, 'something went wrong')
-            else console.log('Success!')
-        })
-        readline.close()
-    })
-    readline.question('Would you like to use a security question for password recovery? (yes/no)', async (choice) => {
-        const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
-        configInfo.security_question = choice == "yes" ? true : false
-        const yamlStr = jsYaml.safeDump({ ...configInfo })
-        fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
-            if (err) console.log(err, 'something went wrong')
-            else console.log('Success!')
-        })
-        readline.close()
-    })
-});
+})
