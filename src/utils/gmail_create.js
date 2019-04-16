74d3c3dd23e74bd3a2c604deb0f1c399373fe827
Josh Byster <joshb1050@gmail.com>
2019-04-15 19:42:42 -0500

    Removed extra part of Gmail create

--- src/utils/gmail_create.js
+++ src/utils/gmail_create.js
@@ -13,7 +13,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), listLabels);
+  authorize(JSON.parse(content), () => {});
 });
 
 /**
@@ -63,28 +63,5 @@ function getNewToken(oAuth2Client, callback) {
       });
       callback(oAuth2Client);
     });
-  });
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listLabels(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
-  gmail.users.labels.list({
-    userId: 'me',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const labels = res.data.labels;
-    if (labels.length) {
-      //console.log('Labels:');
-      //labels.forEach((label) => {
-      //  console.log(`- ${label.name}`);
-      //});
-    } else {
-      console.log('No labels found.');
-    }
   });
 }
