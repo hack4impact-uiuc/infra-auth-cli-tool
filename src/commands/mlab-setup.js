5722d17bfc1a4acf36dcea62ebfd172b7bed20ed
arshiam2 <arshiam2@illinois.edu>
2019-04-22 16:57:31 -0500

    added roles and mlab setup

--- src/commands/mlab-setup.js
+++ src/commands/mlab-setup.js
@@ -11,23 +11,37 @@ module.exports.builder = (yargs) => yargs;
 
 module.exports.handler = handleErrors(async (argv) => {
     let interface = readline.createInterface({
-             input: process.stdin,
-             output: process.stdout
-         })
-    console.log('Enter mLab URI!...')
-    interface.question('What is the URI of the DB?', async (uri) => {
+        input: process.stdin,
+        output: process.stdout
+    })
+    console.log("Head over to https://mlab.com/ and make an account and a database.")
+    console.log("Click on 'Create New' to create a new mongoDB deployment")
+    console.log("Choose Amazon Web Services, Sandbox, us east, give the database a name, and submit the order.")
+    console.log("Create  new user. It will tell you how to add the username and password for your user into a mLab URI")
+    interface.question('Enter your mLab URI', async (uri) => {
         try {
             await mongoose.connect(uri, { useNewUrlParser: true })
             const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
-            configInfo.prodURI = uri
+            console.log(uri)
+            configInfo.prodURI = String(uri)
+            console.log(uri)
             const yamlStr = jsYaml.safeDump({ ...configInfo })
-            fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
-                if (err) console.log(err, 'something went wrong')
-                else console.log('Success!')
+            await fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log('Success!')
+                    interface.close()
+                    process.exit()
+                }
             })
         } catch (e) {
+            console.log(uri)
+            console.log(e)
             console.log('Invalid URI or failure on mongoose\'s end!')
+            interface.close()
+            process.exit()
         }
-        interface.close()
     })
 });
---
+++ src/commands/roles-setup.js
@@ -0,0 +1,90 @@
+const handleErrors = require("../utils/handleErrors");
+const jsYaml = require('js-yaml')
+const fs = require('fs')
+const inquirer = require("inquirer")
+module.exports.command = "setup-roles";
+module.exports.describe =
+    "Customize roles positions";
+module.exports.builder = (yargs) => yargs;
+
+const roles = { "root": null }
+
+
+setRoles = async () => {
+    let permission_level_choices = ["Add a permission level", "Delete a permission level", "Submit permission levels"]
+    let creatingRoles = true
+    while (creatingRoles) {
+        const response = await inquirer
+            .prompt({
+                type: "list",
+                name: "permission_level",
+                message:
+                    "The current permission levels are \n  " + Object.keys(roles).join("     \n  ") + "\n What would you like to do.",
+                choices: permission_level_choices
+            })
+        if (response["permission_level"] === permission_level_choices[0]) {
+            const second_response = await inquirer.prompt({
+                name: 'add',
+                message: "What is the name of your new role",
+
+            });
+            roles[second_response['add']] = null
+
+        }
+        if (response["permission_level"] === permission_level_choices[1]) {
+
+            const second_response = await inquirer.prompt({
+                type: 'list',
+                name: 'delete',
+                message: "What role would you like to delete",
+                choices: Object.keys(roles)
+            });
+            delete roles[second_response['delete']]
+        }
+        if (response["permission_level"] === permission_level_choices[2]) {
+            creatingRoles = false
+        }
+    }
+}
+
+setEachRolesPermissions = async () => {
+    for (role in Object.keys(roles)) {
+        let roleName = Object.keys(roles)[role]
+        const response = await inquirer
+            .prompt({
+                type: "checkbox",
+                name: "permission_level",
+                message:
+                    "The role " + roleName.toUpperCase() + " should be able to promote or demote people within these user permissions. \n For instance, if I'm a root user, I want the ability to promote a guest user to root and visa versa, so I would select both guest and root. \n The lowest permission level should not have anything selected since you don't need permission to sign up for this level. For instance, for the guest role I wouldn't select anything since anyone can sign up as a guest. \n Select ALL roles that the user would be able to promote/demote.",
+                choices: Object.keys(roles)
+            })
+        console.log(response["permission_level"])
+        if ((response["permission_level"].length) != 0) {
+            roles[roleName] = response["permission_level"]
+            console.log("The role " + roleName.toUpperCase() + " can now promote or demote users with the permission levels " + roles[roleName].join(", "))
+        } else {
+            console.log("The role " + roleName.toUpperCase() + " can NOT promote or demote any users")
+        }
+    }
+}
+module.exports.handler = handleErrors(async (argv) => {
+    try {
+        await setRoles();
+        await setEachRolesPermissions();
+    } catch (e) {
+        return console.error(e);
+    }
+    const configInfo = await jsYaml.safeLoad(fs.readFileSync('templates/infra-authentication-server/config/defaultroles.yml', 'utf8'))
+    console.log(roles)
+    configInfo["roles"] = roles
+    const yamlStr = jsYaml.safeDump({ ...configInfo })
+    await fs.writeFile('templates/infra-authentication-server/config/defaultroles.yml', yamlStr, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log('Success!')
+            process.exit()
+        }
+    })
+})
